e578c6817c71c7952aefb2b9f8972810bdac6216
cywhale <cyrain@gmail.com>
2020-07-07 17:58:54 +0800

    try some preactjs basic..

--- node_eco/src/ui/app.js
+++ node_eco/src/ui/app.js
@@ -1,10 +1,23 @@
-/* global htmPreact */
-const { html, Component, render } = htmPreact
+'use strict';
+// if need version htm@3.0.4 or from module //'https://unpkg.com/preact?module';
+import { html, Component, render } from 'https://unpkg.com/htm/preact/standalone.module.js';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.onInput = e => {
+      this.setState({ value: e.target.value });
+    }
+    
+    this.onSubmit = e => {
+      e.preventDefault();
+      this.setState({ name: this.state.value });
+    }  
+  }
+
   async componentDidMount () {
     try {
-      this.setState({ loading: true, time: null })
+      this.setState({ loading: true, time: null, value: '', name: 'user' })
       await fetch('/api/time')
         .then(response => response.json())
         .then(data => this.setState({ loading: false, time: data.time }))
@@ -14,33 +27,36 @@ class App extends Component {
   }
   render (props, state) {
     return html`
-      <div class="container mt-5">
-        <div class="row justify-content-center">
-          <div class="col">
-            <h1>Hello from ECO NODE</h1>
-            <div>
-              ${ state.loading &&
-                  html`
-                    <p>😴 Loading time from server...</p>
-                  `
-              }
-              ${ state.time &&
-                  html`
-                    <p>Site under construction...</p>
-                    <p>⏱ Update : <i>${state.time}</i></p>
-                    <hr />
-                    <p>Just try: </p>
-                    <p> /api/time</p>
-                    <p> /map</p>
-                  `
-              }
-            </div>
-            <hr />
-          </div>
+      <div>
+        <div>
+          <h1>Hello, ${state.name}!</h1>
+          <form onSubmit=${this.onSubmit}>
+            <input type="text" value=${state.value} onInput=${this.onInput} />
+            <button type="submit">Update</button>
+          </form>
         </div>
+        <h3>say hello from ECO NODE</h3>
+        <div>
+          ${ state.loading &&
+            html`
+              <p>😴 Loading time from server...</p>
+            `
+          }
+          ${ state.time &&
+            html`
+              <p>Site under construction...</p>
+              <p>⏱ Update : <i>${state.time}</i></p>
+              <hr />
+                <p>Just try: </p>
+                <p> /api/time</p>
+                <p> /map</p>
+            `
+          }
+        </div>  
+        <hr />
       </div>
     `
   }
 }
 
-render(html`<${App}/>`, document.getElementById('app'))
+render(html`<${App}/>`, document.getElementById('app'));
