18ce3570210a07695105c0ffa0d9c274a411e914
cywhale <cyrain@gmail.com>
2020-07-17 00:22:51 +0800

    firebase build got troubles..

--- node_eco/src/client/components/firebase.js
+++ node_eco/src/client/components/firebase.js
@@ -1,8 +1,8 @@
 //import * as firebase from "firebase/app";
-import { firebase } from '@firebase/app'; // cannot build firebase, but less than 500KB (220KB in size)
-import '@firebase/auth'; // BUT build ERROR Template execution failed: Error: The XMLHttpRequest compatibility library was not found.
-//const firebase = require('firebase'); //can build firebase, but exceed 500KB
-//require('firebase/auth'); //can build, Unable to read file: /node_modules/idb/lib/idb.mjs
+//import { firebase } from '@firebase/app'; // cannot build firebase, but less than 500KB (220KB in size)
+//import '@firebase/auth'; // BUT build ERROR Template execution failed: Error: The XMLHttpRequest compatibility library was not found.
+const firebase = require('firebase'); //can build firebase, but exceed 500KB
+require('firebase/auth'); //can build, Unable to read file: /node_modules/idb/lib/idb.mjs
 // .........................ReferenceError: IDBIndex is not defined
 //import 'firebase/database';
 //import 'firebase/auth';
