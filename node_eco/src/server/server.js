0c923424aa4675c1ce8ebd036fe4fa6061c0e4ba
cywhale <cyrain@gmail.com>
2020-11-10 09:21:36 +0800

    small update fastify@3.8 & pkgs to latest

--- node_eco/src/server/server.js
+++ node_eco/src/server/server.js
@@ -16,14 +16,18 @@ const mongoConnector = require('./db/mongoconn');
 //const argParser = new ArgumentParser({ addHelp: true; });
 //argParser.addArgument(['--port', '-p'], { type: Number, defaultValue: 3000 }
 
-async function configSecServ(certDir='config') {
+const configSecServ = async (certDir='config') => {
   const readCertFile = (filename) => {
     return fs.readFileSync(path.join(__dirname, certDir, filename));
   };
-
-  const [key, cert] = await Promise.all(
+  try {
+    const [key, cert] = await Promise.all(
       [readCertFile('privkey.pem'), readCertFile('fullchain.pem')]);
-  return {key, cert, allowHTTP1: true};
+    return {key, cert, allowHTTP1: true};
+  } catch (err) {
+    console.log('Error: certifite failed. ' + err);
+    process.exit(1);
+  }
 }
 
 const startServer = async (opts) => {
@@ -41,45 +45,62 @@ const startServer = async (opts) => {
       logger: { level: logSeverity }
     });
     //https://web.dev/codelab-text-compression-brotli
-    await server.get('*.js', (req, res, next) => {
-      if (req.header('Accept-Encoding').includes('br')) {
-        req.url = req.url + '.br';
-        console.log(req.header('Accept-Encoding Brotli'));
-        res.set('Content-Encoding', 'br');
-        res.set('Content-Type', 'application/javascript; charset=UTF-8');
-      } else {
-        req.url = req.url + '.gz';
-        console.log(req.header('Accept-Encoding Gzip'));
-        res.set('Content-Encoding', 'gzip');
-        res.set('Content-Type', 'application/javascript; charset=UTF-8');
-      }
-      next();
-    });
+    try {
+      await server.get('*.js', (req, res, next) => {
+        if (req.header('Accept-Encoding').includes('br')) {
+          req.url = req.url + '.br';
+          console.log(req.header('Accept-Encoding Brotli'));
+          res.set('Content-Encoding', 'br');
+          res.set('Content-Type', 'application/javascript; charset=UTF-8');
+        } else {
+          req.url = req.url + '.gz';
+          console.log(req.header('Accept-Encoding Gzip'));
+          res.set('Content-Encoding', 'gzip');
+          res.set('Content-Type', 'application/javascript; charset=UTF-8');
+        }
+        next();
+      });
+    } catch (err) {
+      server.log.error('Error: Accept-encoding fail, ' + err);
+    }
     // register the plugins, routes
     //await server.register(fsAutoPush.staticServe, {root: path.join(__dirname, '..', 'ui')});
     //await server.register(require('fastify-swagger'), swagger.options);
+    try {
+      await server.register(require('fastify-static'), {
+        root: path.join(__dirname, '..', 'ui/build'),
+        prefix: '/',
+        prefixAvoidTrailingSlash: true,
+        list: true /* {
+          format: 'html',
+          names: ['index', 'index.html', '/']
+        }*/
+      });
+    } catch (err) {
+      server.log.error('Error: Serve static ui/build, ' + err);
+    }
 
-    await server.register(require('fastify-static'), {
-      root: path.join(__dirname, '..', 'ui/build'),
-      prefix: '/',
-      prefixAvoidTrailingSlash: true,
-      list: {
-        format: 'html',
-        names: ['index', 'index.html', '/']
-      }
-    });
-
-    await server.register(require('fastify-static'), {
-      root: path.join(__dirname, '..', '..', 'webgl'),
-      prefix: '/includes/',
-      decorateReply: false
-    });
-
-    await server.register(mongoConnector, {uri: mongo_uri});
-
-    await srv_routes.forEach((route, index) => {
+    try {
+      await server.register(require('fastify-static'), {
+        root: path.join(__dirname, '..', '..', 'webgl'),
+        prefix: '/includes/',
+        decorateReply: false
+      });
+    } catch (err) {
+      server.log.error('Error: a trial static(webglearth), ' + err);
+    }
+    try {
+      await server.register(mongoConnector, {uri: mongo_uri});
+    } catch (err) {
+      server.log.error('Error: Mongo connect fail, ' + err);
+    }
+    try {
+      await srv_routes.forEach((route, index) => {
         server.route(route);
-    });
+      });
+    } catch (err) {
+      server.log.error('Error: Serve each route fail, ' + err);
+    }
 
     // start the server
     const start = async () => {
